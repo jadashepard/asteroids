7d2828b51a23cc37aaec2568401797e7e39cbb09
Jada Shepard <jnshepard12@gmail.com>
2024-11-06 20:06:14 -0500

    Create game clock

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
+        #Create game clock and delta time variable
+        clock = pygame.time.Clock()
+        dt = 0
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill('black')
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__=="__main__":
