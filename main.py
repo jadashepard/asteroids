915d71c3399f8ae50db2ca7c4105fe1e8b41b6e0
Jada Shepard <jnshepard12@gmail.com>
2024-11-06 20:27:58 -0500

    Create player

--- main.py
+++ main.py
@@ -3,25 +3,28 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
-# Import constant variables
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # initialize player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    # create game clock and delta time variable
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
-        #Create game clock and delta time variable
-        clock = pygame.time.Clock()
-        dt = 0
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill('black')
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
